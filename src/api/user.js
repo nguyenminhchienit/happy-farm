d3d0c66d5dd58a85eae23472d8cf143d98c54b18
NguyenMinhChien <minhchien03112003@gmail.com>
2024-07-11 21:34:33 +0700

    fix: user api

--- src/api/user.js
+++ src/api/user.js
@@ -1,78 +1,20 @@
 import axios from "../../axios";
 
-const updateUser = (idUser, data) => {
-  return axios.post(`/api/user/editUser/${idUser}`, data);
-};
-const getAllUsers = () => {
-  return axios.get("/api/user/getListUsers");
-};
-
-const getListUsersNotBanned = () => {
-  return axios.get("/api/user/getListUsersNotBanned");
-};
-
-const bannedUser = (idUser) => {
-  return axios.delete(`/api/user/bannedUser/${idUser}`);
-};
+const getAll = () =>{
+    return axios.get("/api/user/getListUsers")
+}
 
 const createUser = (formData) => {
-  return axios.post("/api/user/addNew", formData);
-};
-
-const apiLogin = (username, password) => {
-  return axios({
-    url: `/api/user/login?username=${username}&password=${password}`,
-    method: "post",
-  });
-};
-
-const getCartUser = (id) => {
-  return axios({
-    url: `/api/cart/getlistitem?idUser=${id}`,
-    method: "get",
-  });
-};
-
-const deleteCartUser = (idUser, idProduct) => {
-  return axios({
-    url: `/api/cart/removeitems?idUser=${idUser}&items=${idProduct}`,
-    method: "delete",
-  });
-};
-
-const apiCheckOut = (idUser, data) => {
-  return axios({
-    url: `/api/checkout/getinfouser?idUser=${idUser}`,
-    method: "post",
-    data,
-  });
-};
-
-const apiCreateBlog = (data) => {
-  return axios({
-    url: `/api/blog/addblog`,
-    method: "post",
-    data,
-  });
-};
-
-const apiGetBlog = (id) => {
-  return axios({
-    url: `/api/blog/getblog/${id}`,
-    method: "get",
-  });
-};
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    };
+  
+    return axios.post("/api/user/addNew", formData, config);
+  };
 
 export {
-  apiGetBlog,
-  apiCreateBlog,
-  getAllUsers,
-  createUser,
-  apiLogin,
-  deleteCartUser,
-  getCartUser,
-  apiCheckOut,
-  updateUser,
-  bannedUser,
-  getListUsersNotBanned,
-};
+    getAll  as getAllUsers,
+    createUser 
+}
